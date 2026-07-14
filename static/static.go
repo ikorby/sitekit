6e02ae7707c208d298d6b1b2602667a45cac0ac2
Ikorby <mathsbotan@gmail.com>
2026-07-14 23:22:56 +0300

    feat: add static asset serving

--- static/static.go
+++ static/static.go
@@ -1,1 +1,31 @@
 package static
+
+import (
+	"io/fs"
+	"net/http"
+
+	"github.com/Ikorby/Ikorby-s-Go-Sitekit/config"
+)
+
+func New(fsys fs.FS, cfg *config.Config) http.Handler {
+	fileServer := http.FileServerFS(fsys)
+
+	if cfg.IsProduction() {
+		return withCacheHeaders(fileServer)
+	}
+	return withNoCache(fileServer)
+}
+
+func withCacheHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "public, max-age=31536000, immutable")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func withNoCache(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
+}
