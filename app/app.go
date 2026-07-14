e56bc792c18cb33b168575cf1efcb63abd55fca3
Ikorby <mathsbotan@gmail.com>
2026-07-14 23:19:55 +0300

    feat: implement application core

--- app/app.go
+++ app/app.go
@@ -1,1 +1,33 @@
 package app
+
+import (
+	"errors"
+	"net/http"
+)
+
+type HandlerFunc func(c *Context) error
+
+type ErrorHandler func(c *Context, err error)
+
+var errNoRenderer = errors.New("app: renderer is not configured")
+
+func defaultErrorHandler(c *Context, err error) {
+	if c.W.Header().Get("Content-Type") != "" {
+		return
+	}
+	http.Error(c.W, "Internal Server Error", http.StatusInternalServerError)
+}
+
+func (a *App) Adapt(h HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := newContext(w, r, a.Renderer)
+
+		if err := h(c); err != nil {
+			eh := a.ErrorHandler
+			if eh == nil {
+				eh = defaultErrorHandler
+			}
+			eh(c, err)
+		}
+	}
+}
--- app/context.go
+++ app/context.go
@@ -1,1 +1,50 @@
 package app
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/Ikorby/Ikorby-s-Go-Sitekit/page"
+	"github.com/Ikorby/Ikorby-s-Go-Sitekit/render"
+)
+
+type Context struct {
+	W http.ResponseWriter
+	R *http.Request
+
+	renderer *render.Renderer
+}
+
+func newContext(w http.ResponseWriter, r *http.Request, renderer *render.Renderer) *Context {
+	return &Context{W: w, R: r, renderer: renderer}
+}
+
+func (c *Context) Context() context.Context {
+	return c.R.Context()
+}
+
+func (c *Context) Param(name string) string {
+	return c.R.PathValue(name)
+}
+
+func (c *Context) Query(name string) string {
+	return c.R.URL.Query().Get(name)
+}
+
+func (c *Context) Render(status int, p *page.Page) error {
+	if c.renderer == nil {
+		return errNoRenderer
+	}
+	return c.renderer.Render(c.W, status, p)
+}
+
+func (c *Context) JSON(status int, v any) error {
+	c.W.Header().Set("Content-Type", "application/json; charset=utf-8")
+	c.W.WriteHeader(status)
+	return json.NewEncoder(c.W).Encode(v)
+}
+
+func (c *Context) Redirect(url string, status int) {
+	http.Redirect(c.W, c.R, url, status)
+}
