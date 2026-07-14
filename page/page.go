2b97ae5e47485404dd0aca71d8786e135e2392e8
Ikorby <mathsbotan@gmail.com>
2026-07-14 23:21:40 +0300

    feat: implement page registry

--- page/page.go
+++ page/page.go
@@ -1,1 +1,34 @@
 package page
+
+type Meta struct {
+	Title        string
+	Description  string
+	Keywords     []string
+	CanonicalURL string
+	OGImage      string
+	NoIndex      bool
+}
+
+type Page struct {
+	Template string
+	Layout   string
+	Meta     Meta
+	Data     any
+}
+
+func New(template string, data any) *Page {
+	return &Page{
+		Template: template,
+		Data:     data,
+	}
+}
+
+func (p *Page) WithLayout(layout string) *Page {
+	p.Layout = layout
+	return p
+}
+
+func (p *Page) WithMeta(meta Meta) *Page {
+	p.Meta = meta
+	return p
+}
